b32e9c0b502c65f188aca51c5a9825533a9abb88
emmelf <emmelf.exe@gmail.com>
2024-03-13 15:08:38 +0100

    frontend

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+
+let copyright = document.querySelector(".copyright");
+
+let currdate = new Date();
+
+let yyyy = currdate.getFullYear();
+
+copyright.innerText = `${yyyy} | Dylan Lakhdari`;
